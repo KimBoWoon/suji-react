e81d012c1d203dcea22d339b166449811acdac9e
Secret <secret@localhost.localdomain>
2016-09-19 15:50:49 +0900

    timer

--- src/Aside.js
+++ src/Aside.js
@@ -2,7 +2,6 @@
  * Created by secret on 8/24/16.
  */
 
-
 import React, {Component} from 'react';
 
 class Aside extends Component {
@@ -15,20 +14,20 @@ class Aside extends Component {
             <aside>
                 <div id="sidebar" className="nav-collapse ">
                     <ul className="sidebar-menu" id="nav-accordion">
-                        <p className="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg"
+                        <p className="centered"><a href="profile.html"><img src="assets/img/SUJI_no_profile.png"
                                                                             className="img-circle" width="60"
                                                                             role="presentation"/></a></p>
-                        <h5 className="centered">Marcel Newman</h5>
+                        <h5 className="centered">SUJI</h5>
                         <li className="mt">
                             <a className="active" href="index.html">
-                                <i className="fa fa-dashboard"/>
-                                <span>Dashboard</span>
+                                {/*<i className="fa fa-dashboard"/>*/}
+                                <span>SALES</span>
                             </a>
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;">
-                                <i className="fa fa-desktop"/>
-                                <span>UI Elements</span>
+                                {/*<i className="fa fa-desktop"/>*/}
+                                <span>REGISTER</span>
                             </a>
                             <ul className="sub">
                                 <li><a href="general.html">General</a></li>
@@ -38,8 +37,8 @@ class Aside extends Component {
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;">
-                                <i className="fa fa-cogs"/>
-                                <span>Components</span>
+                                {/*<i className="fa fa-cogs"/>*/}
+                                <span>MANAGEMENT</span>
                             </a>
                             <ul className="sub">
                                 <li><a href="calendar.html">Calendar</a></li>
@@ -47,21 +46,21 @@ class Aside extends Component {
                                 <li><a href="todo_list.html">Todo List</a></li>
                             </ul>
                         </li>
+                        {/*<li className="sub-menu">*/}
+                            {/*<a href="javascript:;">*/}
+                                {/*<i className="fa fa-book"/>*/}
+                                {/*<span>ITEMS</span>*/}
+                            {/*</a>*/}
+                            {/*<ul className="sub">*/}
+                                {/*<li><a href="blank.html">Blank Page</a></li>*/}
+                                {/*<li><a href="login.html">Login</a></li>*/}
+                                {/*<li><a href="lock_screen.html">Lock Screen</a></li>*/}
+                            {/*</ul>*/}
+                        {/*</li>*/}
                         <li className="sub-menu">
                             <a href="javascript:;">
-                                <i className="fa fa-book"/>
-                                <span>Extra Pages</span>
-                            </a>
-                            <ul className="sub">
-                                <li><a href="blank.html">Blank Page</a></li>
-                                <li><a href="login.html">Login</a></li>
-                                <li><a href="lock_screen.html">Lock Screen</a></li>
-                            </ul>
-                        </li>
-                        <li className="sub-menu">
-                            <a href="javascript:;">
-                                <i className="fa fa-tasks"/>
-                                <span>Forms</span>
+                                {/*<i className="fa fa-tasks"/>*/}
+                                <span>CUSTOMER</span>
                             </a>
                             <ul className="sub">
                                 <li><a href="form_component.html">Form Components</a></li>
@@ -69,24 +68,33 @@ class Aside extends Component {
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;">
-                                <i className="fa fa-th"/>
-                                <span>Data Tables</span>
+                                {/*<i className="fa fa-tasks"/>*/}
+                                <span>SETTINGS</span>
+                            </a>
+                            <ul className="sub">
+                                <li><a href="form_component.html">Form Components</a></li>
+                            </ul>
+                        </li>
+                        <li className="sub-menu">
+                            <a href="javascript:;">
+                                {/*<i className="fa fa-th"/>*/}
+                                <span>HELP</span>
                             </a>
                             <ul className="sub">
                                 <li><a href="basic_table.html">Basic Table</a></li>
                                 <li><a href="responsive_table.html">Responsive Table</a></li>
                             </ul>
                         </li>
-                        <li className="sub-menu">
-                            <a href="javascript:;">
-                                <i className=" fa fa-bar-chart-o"/>
-                                <span>Charts</span>
-                            </a>
-                            <ul className="sub">
-                                <li><a href="morris.html">Morris</a></li>
-                                <li><a href="chartjs.html">Chartjs</a></li>
-                            </ul>
-                        </li>
+                        {/*<li className="sub-menu">*/}
+                            {/*<a href="javascript:;">*/}
+                                {/*<i className=" fa fa-bar-chart-o"/>*/}
+                                {/*<span>Charts</span>*/}
+                            {/*</a>*/}
+                            {/*<ul className="sub">*/}
+                                {/*<li><a href="morris.html">Morris</a></li>*/}
+                                {/*<li><a href="chartjs.html">Chartjs</a></li>*/}
+                            {/*</ul>*/}
+                        {/*</li>*/}
                     </ul>
                 </div>
             </aside>
--- src/TopMenu.js
+++ src/TopMenu.js
@@ -4,25 +4,57 @@
 
 import React, {Component} from 'react';
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+import Time from 'react-time'
 import Task from './TaskComponent'
 import Message from './MessageComponent'
 import Login from './Login'
 
 class TopMenu extends Component {
+    getTime() {
+        let today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        // add a zero in front of numbers<10
+        m = this.checkTime(m);
+        s = this.checkTime(s);
+        document.getElementsByClassName('showtime').innerHTML = h + ":" + m + ":" + s;
+        // document.getElementsByClassName('showtime').innerHTML = <h1>Hi</h1>;
+
+        let t = setTimeout(function () {
+            getTime()
+        }, 500);
+    }
+
+    checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
     render() {
+        let now = new Date();
+
         return (
             <header className="header black-bg">
                 <div className="sidebar-toggle-box">
-                    <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip" placement="right">Toggle Navigation</Tooltip>}>
+                    <OverlayTrigger placement="right"
+                                    overlay={<Tooltip id="tooltip" placement="right">Toggle Navigation</Tooltip>}>
                         <div className="fa fa-bars tooltips"/>
                     </OverlayTrigger>
                 </div>
-                <a href="index.html" className="logo"><b>DASHGUM FREE</b></a>
+                <a href="index.html" className="logo"><b>SUJI</b></a>
                 <div className="nav notify-row" id="top_menu">
                     <ul className="nav top-menu">
                         <Task/>
                         <Message/>
                     </ul>
+                </div>
+                <div onLoad={this.getTime()}/>
+                <div className="showtime"/>
+                <div className="nav pull-right top-menu">
+                    <Time value={now} format="HH:mm:ss"/>
                 </div>
                 <Login/>
             </header>
